c5f7d2ff522169897a423d50122a62c5f0da3575
brandon <brandonsoledad7@hotmail.com>
2021-09-23 13:43:12 -0700

    updated MUI with new version

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,68 @@
+import Search from '@mui/icons-material/Search';
 import React from 'react'
+import styled from 'styled-components';
+
+
+const Container = styled.div`
+    height: 60px;
+    
+`;
+const Wrapper = styled.div`
+    padding: 10px 20px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+const Left = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: center;
+`;
+
+const Language = styled.span`
+    font-size: 14px;
+    cursor: pointer;
+`;
+
+const SearchContainer = styled.div`
+    border: 0.5px solid lightgray;
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+    padding: 5px;
+`;
+
+const Input = styled.input`
+    border: none;
+`;
+
+const Center = styled.div`
+    flex: 1;
+`;
+
+const Logo = styled.h1`
+    font-weight: bold;
+`;
+
+const Right = styled.div`
+    flex: 1;
+`;
 
 export default function Navbar() {
     return (
-        <div>
-            navbar
-        </div>
+        <Container> 
+            <Wrapper>
+                <Left>
+                    <Language>EN</Language>
+                    <SearchContainer>
+                        <Input/>
+                        <Search/>
+                    </SearchContainer>
+                </Left>
+                <Center><Logo>SNEAKER VAULT</Logo></Center>
+                <Right>Right</Right>
+            </Wrapper>
+        </Container>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Navbar from '../components/Navbar'
 
 export default function Home() {
     return (
-        <div>
+        <div className="container">
             <Navbar/>
         </div>
     )
