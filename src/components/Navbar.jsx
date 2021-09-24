5f189ed1e469c5dd7acd3d2f3b215f51a4a48968
brandon <brandonsoledad7@hotmail.com>
2021-09-24 13:51:34 -0700

    finished navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
-import Search from '@mui/icons-material/Search';
+import { ShoppingCartOutlined } from '@material-ui/icons';
+import Search from '@mui/icons-material/Search'
+import { Badge } from '@mui/material';
 import React from 'react'
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 
 const Container = styled.div`
@@ -39,6 +41,7 @@ const Input = styled.input`
 
 const Center = styled.div`
     flex: 1;
+    text-align: center;
 `;
 
 const Logo = styled.h1`
@@ -47,6 +50,15 @@ const Logo = styled.h1`
 
 const Right = styled.div`
     flex: 1;
+    display: flex;
+    text-align: center;
+    justify-content: flex-end;
+`;
+
+const MenuItem = styled.div`
+    font-size: 14px;
+    cursor: pointer;
+    margin-left: 25px;
 `;
 
 export default function Navbar() {
@@ -57,11 +69,19 @@ export default function Navbar() {
                     <Language>EN</Language>
                     <SearchContainer>
                         <Input/>
-                        <Search/>
+                        <Search style={{color:"black", fontSize:17}}/>
                     </SearchContainer>
                 </Left>
-                <Center><Logo>SNEAKER VAULT</Logo></Center>
-                <Right>Right</Right>
+                <Center><Logo>SNEAKER VAULT.</Logo></Center>
+                <Right>
+                    <MenuItem>REGISTER</MenuItem>
+                    <MenuItem>SIGN IN</MenuItem>
+                    <MenuItem>
+                        <Badge badgeContent = {4} color = "primary">
+                            <ShoppingCartOutlined/>
+                        </Badge>
+                    </MenuItem>
+                </Right>
             </Wrapper>
         </Container>
     )
