b50e3ef9a2d78456979a3eee6f3f29e3c7ca7b33
Brandon <brandonsoledad7@hotmail.com>
2021-10-09 17:14:37 -0700

    added login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Home from './pages/Home'
+import Login from './pages/Login'
 import Product from './pages/Product'
 import ProductList from './pages/ProductList'
 
 export default function App() {
   return (
-    <Product/>
+    <Login/>
   )
 }
 
