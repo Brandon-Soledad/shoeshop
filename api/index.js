66d53f9bca0d013e639516633b15be8a95cc29ea
Brandon <brandonsoledad7@hotmail.com>
2021-10-20 19:47:21 -0700

    finished stripe payment

--- api/index.js
+++ api/index.js
@@ -4,6 +4,11 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const usersRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -14,10 +19,15 @@ mongoose
         console.log(err);
     });
 
+
+    app.use(cors());
     app.use(express.json());
     app.use("/api/auth", authRoutes);
     app.use("/api/users", usersRoutes);
-    
+    app.use("/api/products", productRoute);
+    app.use("/api/carts", cartRoute);
+    app.use("/api/orders", orderRoute);
+    app.use("/api/checkout", stripeRoute);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Backend server is running!");
