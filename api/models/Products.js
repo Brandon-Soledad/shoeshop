637f4681dc23197c6255581d9a3e00364d62a09c
Brandon <brandonsoledad7@hotmail.com>
2021-10-22 16:09:30 -0700

    changed product api model to list 'brand' instead of 'color'

--- api/models/Products.js
+++ api/models/Products.js
@@ -7,7 +7,7 @@ const productSchema = new mongoose.Schema(
         img: {type: String, required: true},
         categories: {type: Array},
         size: {type: Array},
-        color: {type: Array},
+        brand: {type: Array},
         price: {type: Number, required: true}, 
         inStock: {type: Boolean, default: true},   
     },
