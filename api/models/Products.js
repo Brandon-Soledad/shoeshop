ca9c59c726e1171a612f7aca33cc5f4e5bc9599f
Brandon <brandonsoledad7@hotmail.com>
2021-10-24 19:41:16 -0700

    fixed api now propely logs into correct acc letting admin restricted api calls to work

--- api/models/Products.js
+++ api/models/Products.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema(
-    {
-        title: {type: String, required: true, unique: true},
-        desc: {type: String, required: true,},
-        img: {type: String, required: true},
-        categories: {type: Array},
-        size: {type: String},
-        brand: {type: String},
-        price: {type: Number, required: true}, 
-        inStock: {type: Boolean, default: true},   
-    },
-    {timestamps: true}
+  {
+    title: { type: String, required: true, unique: true },
+    desc: { type: String, required: true, },
+    img: { type: String, required: true },
+    categories: { type: Array },
+    size: { type: String },
+    color: { type: String },
+    price: { type: Number, required: true },
+    
+  },
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("Products", productSchema);
