f0c60af200e361cbe0039dc51ae1cd2dd088e0bb
Brandon <brandonsoledad7@hotmail.com>
2021-11-12 14:00:30 -0800

    update

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ mongoose
     app.use("/api/orders", orderRoute);
     app.use("/api/checkout", stripeRoute);
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.MONGODB_URI || 5000, () => {
     console.log("Backend server is running!");
 })
