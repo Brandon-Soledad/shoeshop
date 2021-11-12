ad797243524280d97f8c770b5dc6101d6becd147
Brandon <brandonsoledad7@hotmail.com>
2021-11-12 13:10:47 -0800

    changed mongo link

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const cors = require("cors");
 dotenv.config();
 
 mongoose
-    .connect(process.env.MONGO_URL)
+    .connect("mongodb+srv://brandon:Pokemon8252@cluster0.lrsm3.mongodb.net/shoeshop?retryWrites=true&w=majority")
     .then(()=> console.log("db connection successful"))
     .catch((err) => {
         console.log(err);
